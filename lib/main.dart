33a7b607d7e1c2dfaf687c971e02a8993bce4d5c
Atilola Oluwatobiloba <tobiatilola@gmail.com>
2022-12-19 11:09:43 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(const XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+
+  Future<void> playSound(int soundNumber) async {
+    final player = AudioPlayer();
+    await player.play(DeviceFileSource('assets/note$soundNumber.wav'));
+  }
+
+  Expanded buildKey(soundNo, keyColor) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          playSound(soundNo);
+        },
+        style: TextButton.styleFrom(
+          backgroundColor: keyColor,
+        ),
+        child: Text(''),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(1, Colors.red),
+              buildKey(2, Colors.orange),
+              buildKey(3, Colors.yellow),
+              buildKey(4, Colors.green),
+              buildKey(5, Colors.teal),
+              buildKey(6, Colors.blue),
+              buildKey(7, Colors.purple),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
